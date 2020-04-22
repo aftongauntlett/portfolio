78489685394c695e3c4c838bfeadfed3d197f222
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-21 21:39:53 -0400

    created a countdown with js aboutme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+var countDownDate = new Date("Jun 16, 2020").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
@@ -17,12 +17,12 @@ var x = setInterval(function () {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+    document.getElementById("gwuEnd").innerHTML = days + " Days <br>" + hours + " Hours<br>"
+        + minutes + " Minutes<br>" + seconds + " Seconds";
 
     // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "Complete!";
+        document.getElementById("gwuEnd").innerHTML = "Complete!";
     }
 }, 1000);
