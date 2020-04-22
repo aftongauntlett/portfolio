184dbb2fa4b38d4de459ce005766c5fd8917bbcb
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-21 21:46:23 -0400

    cleaned up css and js, fixed portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ var x = setInterval(function () {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in the element with id="demo"
+    // Display the result in the element with id=""
     document.getElementById("gwuEnd").innerHTML = days + " Days <br>" + hours + " Hours<br>"
         + minutes + " Minutes<br>" + seconds + " Seconds";
 
